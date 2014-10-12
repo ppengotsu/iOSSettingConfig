5f10f17a07d0efc6855ce6ccce4c883842fbc37d
ppengotsu <ppengotsu.renrakusaki@gmail.com>
2014-10-12 23:25:41 +0900

    サンプルコード作成

--- PPESettingConfig/PPESettingConfig/ViewController.h
+++ PPESettingConfig/PPESettingConfig/ViewController.h
@@ -8,7 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UISwitch *showDebugInfoSw;//デバッグ情報表示するかスイッチ
+    IBOutlet UILabel *connectProtocolLb;//接続するプロトコル
+    IBOutlet UILabel *connectAddressLb;//接続するアドレス
+    IBOutlet UISwitch *overWriteUserIDSw;//ユーザIDを上書きするかどうか
+    IBOutlet UILabel *overWriteUserIDLb;//上書きするユーザID
+    IBOutlet UISwitch *allResetSw;//全てリセットスイッチ
+    
+}
 
 
 @end
--- PPESettingConfig/PPESettingConfig/ViewController.m
+++ PPESettingConfig/PPESettingConfig/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PPESettingsManager.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //PPESettingManagerの初期設定
+    PPESettingsManager *setting = [PPESettingsManager sharedInstance];//シングルトンパターンで、インスタンス生成
+    
+    [setting setUserID:@"0000111"];//ユーザID
+    [setting setUserName:@"hoge"];//ユーザ名
+    
+    BOOL showDebugInfoFlag = [setting acceptsShowDebugInfoFlag];//デバッグ情報を表示するかのフラグ
+    NSString *connectProtocol = [setting connectProtocol];//接続するプロトコル
+    NSString *connectAddress = [setting connectAddress];//接続するアドレス
+    BOOL overwriteUserIDFlag = [setting acceptsOverWriteUserID];//ユーザIDを上書きするかどうか
+    NSString *overwriteUserID = [setting overWriteUserID];//上書きするユーザID
+    BOOL allResetFlag = [setting acceptAllReset];//全てリセットするかのフラグ
+    
+    
+    //ここから下は、画面に表示してわかりやすくしているだけ
+    showDebugInfoSw.on = showDebugInfoFlag;//デバッグ情報表示
+    
+    connectProtocolLb.text = connectProtocol;//接続プロトコル
+    
+    connectAddressLb.text = connectAddress;//接続アドレス
+    
+    overWriteUserIDSw.on = overwriteUserIDFlag;//ユーザID上書きするかどうか
+    
+    overWriteUserIDLb.text = overwriteUserID;//上書きするユーザID
+    
+    allResetSw.on = allResetFlag;//全てリセットするかどうか
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
